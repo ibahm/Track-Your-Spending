def29b5f52637b8a4489b3d0e2744ab185bdb769
Ibahm <ibrahimahm92@gmail.com>
2022-08-06 03:42:20 +0100

    Removed redundant information and options
    - Removed the budget option
    - 'Total' component now styles with a green or red background depending on amount

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,18 @@
 import React from "react";
-import { VscEdit } from "react-icons/vsc";
 import {GlobalContext} from "../context/GlobalState";
 import {useContext} from "react";
 
 const NavBar = () => {
     const {transactions} = useContext(GlobalContext);
-
     const prices = transactions.map(transaction => transaction.price);
-
     const total = prices.reduce((pVal, cVal) => (pVal + cVal)).toFixed(2);
 
     return (
         <div className="nav-bar">
             <h1 className="nav-title">Track Your Spending</h1>
             <ul>
-                <li className="budget-container">
-                    <span className = "budget">£5000</span>
-                    <VscEdit className="edit-budget"/>
-                </li>
                 <li className="item-2"><span className="expense">£5500</span></li>
-                <li className="item-3"><span className="total">£{total}</span></li>
+                <li className={total < 0 ? "total-red":"total-green"}><span className="total" >£{total}</span></li>
             </ul>
         </div>
     );
