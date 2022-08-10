dc006ac5cdfbe911118cc0bc3a1cb170daa54dc7
Ahmed <56367176+ibahm@users.noreply.github.com>
2022-08-10 19:35:48 +0100

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,6 @@ const NavBar = () => {
     const {transactions} = useContext(GlobalContext);
     const prices = transactions.map(transaction => transaction.price);
 
-    //Add to document understanding of conditional statements
     const total = prices.reduce((pVal, cVal) => (pVal + cVal), 0).toFixed(2);
     const income = prices.filter(cVal => cVal > 0).reduce((pVal, cVal) => (pVal += cVal), 0).toFixed(2);
     const expense = prices.filter(cVal => cVal < 0).reduce((pVal, cVal) => (pVal += cVal), 0).toFixed(2);
